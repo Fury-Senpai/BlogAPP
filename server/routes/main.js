5ae430ffe4ce5cd45244c9e9135b0eb61c41cdb8
Fury-Senpai <harbola03nitish@gmail.com>
2025-06-26 01:15:31 +0530

    update

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router();
 const postModel = require('../models/post');
+
 //home page
-router.get('/',(req,res)=>{
-    res.render("index");
+router.get('/',async(req,res)=>{
+    try {
+        const post = await postModel.find();
+        res.render("index",{post});
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 router.get('/about',(req,res)=>{
